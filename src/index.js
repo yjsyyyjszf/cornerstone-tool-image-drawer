4f82a7167e4c3bd97c8b6a68610362d98493e3b7
Pierre Le Guen <pierre@leguen.fr>
2020-03-03 15:27:41 +0100

    implement functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import cornerstone from 'cornerstone-core';
+// import cornerstone from 'cornerstone-core';
 import cornerstoneTools from 'cornerstone-tools';
+
+const { cornerstone } = window;
 
 const BaseAnnotationTool = cornerstoneTools.import('base/BaseAnnotationTool');
 const drawPath = cornerstoneTools.import('drawing/path');
 const getNewContext = cornerstoneTools.import('drawing/getNewContext');
 const moveNewHandle = cornerstoneTools.import('manipulators/moveNewHandle');
+const anyHandlesOutsideImage = cornerstoneTools.import(
+  'manipulators/anyHandlesOutsideImage'
+);
+const EVENTS = cornerstoneTools.EVENTS;
 
 export default class ImageDrawerTool extends BaseAnnotationTool {
   constructor(props = {}) {
@@ -25,23 +31,6 @@ export default class ImageDrawerTool extends BaseAnnotationTool {
     this.img.src = 'https://image.flaticon.com/icons/svg/2531/2531249.svg';
   }
 
-  enabledCallback() {
-    this._forceImageUpdate();
-  }
-
-  disabledCallback() {
-    this._forceImageUpdate();
-  }
-
-  _forceImageUpdate() {
-    const enabledElement = cornerstone.getEnabledElement(this.element);
-    const hasImageToUpdate = enabledElement.image;
-
-    if (hasImageToUpdate) {
-      cornerstone.updateImage(this.element, true);
-    }
-  }
-
   addNewMeasurement(evt) {
     const eventData = evt.detail;
     const {element, image} = eventData;
@@ -49,11 +38,37 @@ export default class ImageDrawerTool extends BaseAnnotationTool {
     const measurementData = this.createNewMeasurement(eventData);
 
     cornerstoneTools.addToolState(element, this.name, measurementData); //addToolState(element, toolType, measurementData)
-    const { end } = measurementData.handles;
-    cornerstone.updateImage(element);
+    const {end} = measurementData.handles;
 
-    console.log(image);
+    cornerstone.updateImage(this.element);
 
+
+    moveNewHandle(
+      eventData,
+      this.name,
+      measurementData,
+      end,
+      {},
+      'mouse',
+      () => {
+        if (anyHandlesOutsideImage(eventData, measurementData.handles)) {
+          // Delete the measurement
+          cornerstoneTools.removeToolState(element, this.name, measurementData);
+        } else {
+          const center = getCenter(measurementData.handles);
+
+          measurementData.handles.perpendicularPoint.x = center.x;
+          measurementData.handles.perpendicularPoint.y = center.y;
+          measurementData.handles.perpendicularPoint.isFirst = false;
+          this.updateCachedStats(image, element, measurementData);
+          cornerstone.triggerEvent(
+            element,
+            EVENTS.MEASUREMENT_COMPLETED,
+            measurementData
+          );
+        }
+      }
+    );
   }
 
   createNewMeasurement(eventData) {
@@ -133,12 +148,47 @@ export default class ImageDrawerTool extends BaseAnnotationTool {
       // TODO(pleguen): error handle
     }
 
-    if (data.visible == false) {
+    if (data.visible === false) {
       return false;
     }
 
     // const dist = interactionType === 'mouse' ? 15 : 25;
-    console.log(interactionType);
+  }
+
+  mouseMoveCallback(evt) {
+    const eventData = evt.detail;
+    const { element } = eventData;
+
+    const toolData = cornerstoneTools.getToolState(element, this.name);
+
+    if (!toolData) {
+      return
+    }
+
+    // We have tool data, search through all data
+    // And see if we can activate a handle
+    let imageNeedsUpdate = false;
+
+    // TODO(pleguen): implement functions when mouse moves over the canvas
+  }
+
+  handleSelectedCallback(evt) {
+    const eventData = evt.detail;
+    const { element } = eventData;
+    console.log('handleSelectedCallback', element);
   }
 
 }
+
+const getCenter = (handles) => {
+  const {start, end} = handles;
+  const w = Math.abs(start.x - end.x);
+  const h = Math.abs(start.y - end.y);
+  const xMin = Math.min(start.x, end.x);
+  const yMin = Math.min(start.y, end.y);
+
+  return {
+    x: xMin + w / 2,
+    y: yMin + h / 2,
+  };
+};
